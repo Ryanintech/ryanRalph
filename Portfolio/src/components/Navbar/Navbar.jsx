12d1f7e8522c4ababde5931afe156a51b1a24ecc
Ryanintech <164881566+Ryanintech@users.noreply.github.com>
2024-11-07 15:11:58 +0000

    Started working on the Portfolio

--- Portfolio/src/components/Navbar/Navbar.jsx
+++ Portfolio/src/components/Navbar/Navbar.jsx
@@ -2,7 +2,17 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
-  return <div className="navbar"> Sup</div>;
+  return (
+    <div className="navbar">
+      <ul className="nav-menu">
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Skills</li>
+        <li>Contact Me</li>
+      </ul>
+    </div>
+  );
 };
 
 export default Navbar;
