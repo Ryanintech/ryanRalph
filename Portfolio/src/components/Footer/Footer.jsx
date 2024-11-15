35c2352fdd98282c29cc53dbdb84e3c63f9c8e74
Ryanintech <164881566+Ryanintech@users.noreply.github.com>
2024-11-15 13:57:26 +0000

    Added a footer

---
+++ Portfolio/src/components/Footer/Footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <div>
+      <footer className="footer">
+        <div className="footer-text">
+          <p>Copyright &copy; 2024 by Ryan Ralph | All Rights Reserved.</p>
+        </div>
+      </footer>
+    </div>
+  );
+};
+
+export default Footer;
